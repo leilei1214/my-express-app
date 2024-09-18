fdfdb7290c038fbfc42bdb9704da6ad02c41e3f6
unknown <a0933252747@gmail.com>
2024-09-18 15:51:45 +0800

    Install express module

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
+// 提供 public 文件夹中的静态文件
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
+// 登录页面路由
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+app.get('/login_data', (req, res) => {
+  const client_id = "1661291645";
+  const response_type = "code";
+  const redirect_uri = "http://localhost:3000/callback";
+  const state = generateState();
+  const LineLoginUrl = `https://access.line.me/oauth2/v2.1/authorize?scope=profile%20openid&client_id=${client_id}&response_type=${response_type}&redirect_uri=${redirect_uri}&state=${state}`;
+  res.redirect(LineLoginUrl);
+});
+function generateState() {
+  return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+}
+const NewGuid = function () {
+    return (g() + g() + "-" + g() + "-" + g() + "-" + g() + "-" + g() + g() + g());
+}
