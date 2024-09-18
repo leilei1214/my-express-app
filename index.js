c7f2a5ebb8e38c47c44756d11050e1d55f6c3619
unknown <a0933252747@gmail.com>
2024-09-18 18:10:34 +0800

    Install express module

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Path to your service account key JSON file
 const serviceAccount = require('./config/serviceAccountKey.json');
