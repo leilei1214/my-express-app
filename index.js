12097bfeb0bdcc6a59c7a1c5cf86a9af76aa7057
leilei1214 <a0933252747@gmail.com>
2025-06-28 00:36:18 +0800

    Initial commit

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ const pool = mysql.createPool({
   password: '@Aa0918625729',
   database: 'football'
 });
-module.exports = pool;
+const query = util.promisify(pool.query).bind(pool);
+
 // 生成随机的 identifier（与之前一致）
 function generateIdentifier() {
   const digits = Math.floor(1000 + Math.random() * 9000).toString();
@@ -174,45 +175,37 @@ app.get('/login_data', async (req, res) => {
           console.log(userId)
           console.log(displayName)  
       
-          // res.status(299).send('An error occurred');
-          pool.query('SELECT * FROM users WHERE userID = ?', [userId], (error, results) => {
-            if (error) {
-              console.error('MySQL query error:', error);
-              res.status(500).json({ error: 'Database query failed', details: error.message })              
-            } else {
-              res.status(200).json(results);
-            }
-          });
-          // try {
 
-          //   const result = await client.query('SELECT * FROM users WHERE userID = $1', [userId]);
+          try {
+
+            const result = await query('SELECT * FROM users WHERE userID = ?', [userId]);
     
-          //   if (result.rows.length > 0) {
-          //     // User exists, redirect to homepage
-          //     const user = result.rows[0]; // 取出第一条记录
-          //     const displayName = user.displayName; // 读取 username 列
-          //     const identifier = user.identifier; // 读取 email 列
-          //     const birthday = user.birthday; // 读取 email 列
-          //     const position1 = user.preferred_position1; // 读取 email 列
-          //     const position2 = user.preferred_position2; // 读取 email 列
-          //     const level = user.level; 
-          //     req.session.user = { displayName, identifier,birthday,position1,position2,level };
+            if (result.rows.length > 0) {
+              // User exists, redirect to homepage
+              const user = result.rows[0]; // 取出第一条记录
+              const displayName = user.displayName; // 读取 username 列
+              const identifier = user.identifier; // 读取 email 列
+              const birthday = user.birthday; // 读取 email 列
+              const position1 = user.preferred_position1; // 读取 email 列
+              const position2 = user.preferred_position2; // 读取 email 列
+              const level = user.level; 
+              req.session.user = { displayName, identifier,birthday,position1,position2,level };
 
-          //     res.redirect('/');
-          //   } else {
-          //     const identifier = await generateUniqueIdentifier(client); // 生成唯一的 identifier
-          //     const userSession = req.session.user;
-          //     const { birthday, position1, position2} = userSession;
-          //     // Insert new user into PostgreSQL database
-          //     await client.query(
-          //       'INSERT INTO users (username, userid, identifier,birthday,preferred_position1,preferred_position2) VALUES ($1, $2,$3,$4,$5,$6)',
-          //       [displayName, userId,identifier,birthday,position1,position2]
-          //     );
-          //     res.redirect('/');
-          //   }
-          // } finally {
-          //   client.release();
-          // }
+              res.redirect('/');
+            } else {
+              const identifier = await generateUniqueIdentifier(client); // 生成唯一的 identifier
+              const userSession = req.session.user;
+              const { birthday, position1, position2} = userSession;
+              // Insert new user into PostgreSQL database
+              await client.query(
+                'INSERT INTO users (username, userid, identifier,birthday,preferred_position1,preferred_position2) VALUES (?,?,?,?,?,?)',
+                [displayName, userId,identifier,birthday,position1,position2]
+              );
+              res.redirect('/');
+            }
+          } finally {
+            client.release();
+          }
           
  
     
