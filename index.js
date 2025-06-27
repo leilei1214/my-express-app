248595fda27a04ee0e78e24463cb623a68956417
leilei1214 <a0933252747@gmail.com>
2025-06-28 01:21:41 +0800

    Initial commit

--- index.js
+++ index.js
@@ -178,33 +178,33 @@ app.get('/login_data', async (req, res) => {
 
           try {
 
-            const result = await MS_query('SELECT * FROM users WHERE userid = ?', [userId]);
+            // const result = await MS_query('SELECT * FROM users WHERE userid = ?', [userId]);
             console.log(182)
-            if (result.rows.length > 0) {
-              // User exists, redirect to homepage
-              const user = result.rows[0]; // 取出第一条记录
-              const displayName = user.displayName; // 读取 username 列
-              const identifier = user.identifier; // 读取 email 列
-              const birthday = user.birthday; // 读取 email 列
-              const position1 = user.preferred_position1; // 读取 email 列
-              const position2 = user.preferred_position2; // 读取 email 列
-              const level = user.level; 
-              req.session.user = { displayName, identifier,birthday,position1,position2,level };
+            // if (result.rows.length > 0) {
+            //   // User exists, redirect to homepage
+            //   const user = result.rows[0]; // 取出第一条记录
+            //   const displayName = user.displayName; // 读取 username 列
+            //   const identifier = user.identifier; // 读取 email 列
+            //   const birthday = user.birthday; // 读取 email 列
+            //   const position1 = user.preferred_position1; // 读取 email 列
+            //   const position2 = user.preferred_position2; // 读取 email 列
+            //   const level = user.level; 
+            //   req.session.user = { displayName, identifier,birthday,position1,position2,level };
 
-              res.redirect('/');
-            } else {
-              console.log(196)
-              const identifier = await generateUniqueIdentifier(client); // 生成唯一的 identifier
-              const userSession = req.session.user;
-              const { birthday, position1, position2} = userSession;
-              // Insert new user into PostgreSQL database
-              console.log(201)
-              await MS_query(
-                'INSERT INTO users (username, userid, identifier,birthday,preferred_position1,preferred_position2) VALUES (?,?,?,?,?,?)',
-                [displayName, userId,identifier,birthday,position1,position2]
-              );
-              res.redirect('/');
-            }
+            //   res.redirect('/');
+            // } else {
+            //   console.log(196)
+            //   const identifier = await generateUniqueIdentifier(client); // 生成唯一的 identifier
+            //   const userSession = req.session.user;
+            //   const { birthday, position1, position2} = userSession;
+            //   // Insert new user into PostgreSQL database
+            //   console.log(201)
+            //   await MS_query(
+            //     'INSERT INTO users (username, userid, identifier,birthday,preferred_position1,preferred_position2) VALUES (?,?,?,?,?,?)',
+            //     [displayName, userId,identifier,birthday,position1,position2]
+            //   );
+            //   res.redirect('/');
+            // }
           }
           catch(error){
             console.error('Error:', error);
