feae7fbea8a606ab0d614e781b6f49d3a543fb76
leilei1214 <a0933252747@gmail.com>
2025-06-29 00:19:28 +0800

    Initial commit

--- add_activity.js
+++ add_activity.js
@@ -1,16 +1,16 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: 'neondb_owner',
-    host: 'ep-damp-rain-a85ghmem-pooler.eastus2.azure.neon.tech',
-    database: 'neondb',
-    password: 'npg_BORmjcDE8l1P',
-    port: 5432,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
-const formatArrayForPostgres = (arr) => `{${arr.join(',')}}`;
+// const pool = new Pool({
+//     user: 'neondb_owner',
+//     host: 'ep-damp-rain-a85ghmem-pooler.eastus2.azure.neon.tech',
+//     database: 'neondb',
+//     password: 'npg_BORmjcDE8l1P',
+//     port: 5432,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     },
+// });
+const formatArrayForPostgres = (arr) => `{${arr.join(',')}}`;  
 
 const handleActivitySubmission = async (req, res) => {
     const eventData = req.body;
@@ -18,43 +18,43 @@ const handleActivitySubmission = async (req, res) => {
 
   
 
-    let client;
-    const query = `
-    INSERT INTO activities (
-        activity_level, 
-        time, 
-        activity_notice, 
-        activity_intro, 
-        max_participants, 
-        phone, 
-        amount, 
-        location
-    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-    RETURNING *;
-    `;
+    // let client;
+    // const query = `
+    // INSERT INTO activities (
+    //     activity_level, 
+    //     time, 
+    //     activity_notice, 
+    //     activity_intro, 
+    //     max_participants, 
+    //     phone, 
+    //     amount, 
+    //     location
+    // ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+    // RETURNING *;
+    // `;
 
-    const values = [
-        formatArrayForPostgres(eventData.activity_level), // 轉換成 '{新手,復健}'
-        eventData.date || null,
-        eventData.activity_notice || null,
-        eventData.activity_intro || null,
-        eventData.max_participants || null,
-        eventData.phone || null,
-        eventData.amount || null,
-        eventData.address || null
-    ];
+    // const values = [
+    //     formatArrayForPostgres(eventData.activity_level), // 轉換成 '{新手,復健}'
+    //     eventData.date || null,
+    //     eventData.activity_notice || null,
+    //     eventData.activity_intro || null,
+    //     eventData.max_participants || null,
+    //     eventData.phone || null,
+    //     eventData.amount || null,
+    //     eventData.address || null
+    // ];
 
     try {
-        const client = await pool.connect();
-        const result = await client.query(query, values);
-        console.log('Insert success:', result.rows[0]); // 檢查插入結果
-        res.status(200).json({ status: 200, data: result.rows[0] });
+        // const client = await pool.connect();
+        // const result = await client.query(query, values);
+        // console.log('Insert success:', result.rows[0]); // 檢查插入結果
+        // res.status(200).json({ status: 200, data: result.rows[0] });
+        res.status(200).json({ status: 200 });
+
     } catch (err) {
         console.error('資料庫插入失敗:', err);
         res.status(500).json({ status: 500, message: '資料庫插入錯誤' });
-    } finally {
-        if (client) client.release();
-    }
+    } 
 };
 
 
